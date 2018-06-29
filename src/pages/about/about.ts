cb890aec4b84bbc26fd27e875b5468b85f37d620
ihahachi <iha2007hachi@gmail.com>
2018-06-29 19:59:23 +0100

    Read list doctors from Firbase

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase, AngularFireObject } from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import { Doctors } from '../../model/doctors';
+import { DoctorServiceProvider } from '../../providers/doctor-service/doctor-service';
+
+
+
 
 @Component({
   selector: 'page-about',
@@ -7,8 +14,58 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
-
+  doctors: Doctors = {
+    latitude: '',
+    longitude: '',
+    info: ''
   }
+
+  itemArray = [];
+  myObject = []
+  items: AngularFireObject<any>;
+
+
+
+
+  constructor(public navCtrl: NavController,
+    public db: AngularFireDatabase,
+    public doctorServiceProvider: DoctorServiceProvider) {
+
+      this.FillData();
+  }
+
+
+  doRefresh() {
+
+    this.FillData();
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      
+    }, 2000);
+  }
+
+
+
+  FillData() {
+    this.items = this.db.object('doctorsmap');
+    this.items.snapshotChanges().subscribe(action => {
+
+      if (action.payload.val() == null || action.payload.val() == undefined) {
+        console.log('no data')
+      } else {
+
+        this.itemArray.push(action.payload.val())
+        this.myObject = Object.entries(this.itemArray[0])
+
+      }
+
+    });
+  }
+
+
+
+
+
 
 }
