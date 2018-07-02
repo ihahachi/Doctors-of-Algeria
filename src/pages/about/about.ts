5b915285dec0943d3cafc17b214b5d5586beaa5e
ihahachi <iha2007hachi@gmail.com>
2018-07-02 14:40:08 +0100

    Fix problem (BG- Loading).

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -32,54 +32,58 @@ export class AboutPage {
     public db: AngularFireDatabase,
     public doctorServiceProvider: DoctorServiceProvider,
     public loadingCtrl: LoadingController) {
-    this.presentLoadingDefault();
     this.FillData();
+    this.presentLoadingCustom();
   }
 
 
-  doRefresh() 
-  {
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
     this.FillData();
-
-    setTimeout(() =>
-     {
-        console.log('Async operation has ended');
-     }, 2000);
-  }
-
-
-
-  FillData() 
-  {
-    this.items = this.db.object('doctorsmap');
-    this.items.snapshotChanges().subscribe(action => {
-
-      if (action.payload.val() == null || action.payload.val() == undefined)
-       {
-          console.log('no data')
-       } else 
-       {
-          this.itemArray.push(action.payload.val())
-          this.myObject = Object.entries(this.itemArray[0])
-      }
-    });
-  }
-
-
-  presentLoadingDefault() {
-    let loading = this.loadingCtrl.create({
-      content: 'جاري تحميل البيانات '
-    });
-  
-    loading.present();
-  
+    this.presentLoadingCustom();
     setTimeout(() => {
-      loading.dismiss();
+      console.log('Async operation has ended');
+      refresher.complete();
     }, 2000);
   }
 
 
+  // Get Data From Firbase.
+  FillData() {
+    this.items = this.db.object('doctorsmap');
+    this.items.snapshotChanges().subscribe(action => {
 
+      if (action.payload.val() == null || action.payload.val() == undefined) {
+        console.log('no data')
+      } else {
+        this.itemArray.push(action.payload.val())
+        this.myObject = Object.entries(this.itemArray[0])
+      }
+    });
+  }
+
+  
+
+
+//Loanding function.
+  presentLoadingCustom() {
+    let loading = this.loadingCtrl.create({
+      spinner: 'ios',
+      content: `
+        <div class="custom-spinner-container">
+        <p>جاري تحميل البيانات</p>
+        <ion-spinner name="ios"></ion-spinner>
+        </div>`,
+      duration: 1000
+    });
+  
+    loading.onDidDismiss(() => {
+      console.log('Dismissed loading');
+    });
+  
+    loading.present();
+  }
 
 
 
