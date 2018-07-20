b37d1975e3650f4d437dee828a527b9e8fcdb005
ihahachi <iha2007hachi@gmail.com>
2018-07-20 16:26:04 +0100

    add postion detail

--- src/pages/doctorsmap/doctorsmap.ts
+++ src/pages/doctorsmap/doctorsmap.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ViewChild , ElementRef } from '@angular/core'
+import { ViewChild, ElementRef } from '@angular/core'
 
-declare let google:any
+declare let google: any
 
 //@IonicPage()
 @Component({
@@ -11,35 +11,53 @@ declare let google:any
 })
 
 export class DoctorsmapPage {
-  @ViewChild('map') mapElement:ElementRef
-  map:any
-  title : string;
+  @ViewChild('map') mapElement: ElementRef
+  map: any
+  title: string;
   longitude: string;
-  latitude:string;
+  latitude: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.title=navParams.get('title');
-    this.longitude=navParams.get('longitude');
-    this.latitude=navParams.get('latitude');
+    this.title = navParams.get('title');
+    this.longitude = navParams.get('longitude');
+    this.latitude = navParams.get('latitude');
   }
 
   ionViewDidLoad() {
-     this.loadMap()
+    this.loadMap()
   }
 
 
-loadMap(){
-      let LatLng = new google.maps.LatLng(34.6692 , 3.2539  );
-      let mapOptions = {
-        center:LatLng,
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-     }
-     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-     let marker = new google.maps.Marker({
+  loadMap() {
+
+    //add map postion start
+    let LatLng = new google.maps.LatLng(34.6692, 3.2539);
+    let mapOptions = {
+      center: LatLng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    //load map for current options
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    //add Marker postion
+    let marker = new google.maps.Marker({
       map: this.map,
       animation: google.maps.Animation.DROP,
-      position: new google.maps.LatLng(this.latitude,this.longitude)
+      position: new google.maps.LatLng(this.latitude, this.longitude)
     });
-    }
+
+    //add Marker info
+    marker.info = new google.maps.InfoWindow({
+      content: this.title
+    })
+    
+    //create event watching click maker
+    google.maps.event.addListener(marker , 'click' , function(){
+      let marker_map = this.getMap();
+      this.info.open(marker_map,this)
+      alert(this.info.content)
+    });
+  }
 }
