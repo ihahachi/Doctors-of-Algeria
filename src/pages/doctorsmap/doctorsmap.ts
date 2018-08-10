be7bf71962d82ba16c3539a528ac75a3f9a881c7
ihahachi <iha2007hachi@gmail.com>
2018-08-10 16:24:45 +0100

    Add Service API Get directions

--- src/pages/doctorsmap/doctorsmap.ts
+++ src/pages/doctorsmap/doctorsmap.ts
@@ -14,32 +14,60 @@ export class DoctorsmapPage {
   @ViewChild('map') mapElement: ElementRef
   map: any
   title: string;
+
   longitude: string;
   latitude: string;
+
   
-    constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.title = navParams.get('title');
     this.longitude = navParams.get('longitude');
     this.latitude = navParams.get('latitude');
   }
 
   ionViewDidLoad() {
-    this.loadMap()
+    this.loadMap();
+    //this.calcRoute();
   }
 
 
   loadMap() {
+    // definied  directionsDisplay and directionsService
+    var directionsDisplay = new google.maps.DirectionsRenderer();
+    var directionsService = new google.maps.DirectionsService();
+    
+   let LatLng = new google.maps.LatLng(34.6675456, 3.25176);
+   let End = new google.maps.LatLng(this.latitude, this.longitude);
 
     //add map postion start
-    let LatLng = new google.maps.LatLng(34.6692, 3.2539);
     let mapOptions = {
       center: LatLng,
       zoom: 13,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
+    // Add two points origin, destination
+    var request = {
+      origin: LatLng,
+      destination: End,
+      travelMode: 'WALKING'
+    };
+
+    // Set directions route
+    directionsService.route(request, function (response, status) {
+      if (status == 'OK') {
+        directionsDisplay.setDirections(response);
+      }
+    });
+
     //load map for current options
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    //Show Directions into current map
+    directionsDisplay.setMap(this.map);
+
 
     //add Marker postion
     let marker = new google.maps.Marker({
@@ -52,12 +80,12 @@ export class DoctorsmapPage {
     marker.info = new google.maps.InfoWindow({
       content: this.title
     })
-    
+
     //create event watching click maker
-    google.maps.event.addListener(marker , 'click' , function(){
+    google.maps.event.addListener(marker, 'click', function () {
       let marker_map = this.getMap();
-      this.info.open(marker_map,this)
+      this.info.open(marker_map, this)
       alert(this.info.content)
     });
   }
-}
+}  
